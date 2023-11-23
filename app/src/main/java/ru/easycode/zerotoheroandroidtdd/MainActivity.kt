417be6c4b19fe3b3a444e63b6e3b65bdc202c379
spetrenko <spetrenko@zeushq.com>
2023-11-23 16:24:23 +0200

    Task 8 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,42 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var titleTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        titleTextView = findViewById(R.id.titleTextView)
+        initHideButton()
+    }
+
+    private fun initHideButton() {
+        val hideButton: Button = findViewById(R.id.hideButton)
+        hideButton.setOnClickListener {
+            if (titleTextView.isVisible)
+                titleTextView.visibility = View.GONE
+            else
+                titleTextView.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(TITLE_VISIBILITY_KEY, titleTextView.visibility)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        titleTextView.visibility = savedInstanceState.getInt(TITLE_VISIBILITY_KEY)
+
+    }
+
+    companion object {
+        private const val TITLE_VISIBILITY_KEY = "titleVisibilityKey"
     }
 }
