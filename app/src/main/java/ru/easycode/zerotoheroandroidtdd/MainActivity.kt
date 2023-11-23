19e59604c195d1b36dfe1c5e005ef534603b64b9
spetrenko <spetrenko@zeushq.com>
2023-11-23 17:16:19 +0200

    Task 9 solved by oop

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -8,33 +8,32 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var titleTextView: TextView
-    private lateinit var rootLayout:LinearLayout
+    private lateinit var rootLayout: LinearLayout
+    private var state : State = State.Initial
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         titleTextView = findViewById(R.id.titleTextView)
-        val removeButton : Button = findViewById(R.id.removeButton)
+        val removeButton: Button = findViewById(R.id.removeButton)
         rootLayout = findViewById(R.id.rootLayout)
 
         removeButton.setOnClickListener {
-            rootLayout.removeView(titleTextView)
+            state = State.Removed
+            state.apply(linearLayout = rootLayout, textView = titleTextView)
         }
-
 
 
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        val isTitleRemoved = rootLayout.childCount == 1
-        outState.putBoolean(KEY,isTitleRemoved)
+        outState.putSerializable(KEY, state)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        val isTitleRemoved = savedInstanceState.getBoolean(KEY)
-        if(isTitleRemoved)
-            rootLayout.removeView(titleTextView)
+        state = savedInstanceState.getSerializable(KEY) as State
+        state.apply(linearLayout = rootLayout, textView = titleTextView)
     }
 
     companion object {
